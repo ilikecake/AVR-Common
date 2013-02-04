566f72f8ee1fc300adec50ed8efd54cb1dc278a7
Pat S <git@satyshur.net>
2013-02-03 21:05:03 -0600

    Added a file to store common type definitions

---
+++ common_types.h
@@ -0,0 +1,40 @@
+/*   This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/** \file
+*	\brief		Commonly used type definitions
+*	\author		Pat Satyshur
+*	\version	1.0
+*	\date		2/3/2013
+*	\copyright	Copyright 2013, Pat Satyshur
+*	\ingroup 	common
+*
+*	@{
+*/
+
+#ifndef _COMMON_TYPES_H_
+#define _COMMON_TYPES_H_
+
+typedef struct TimeAndDate 
+{
+	int year;
+	int month;
+	int day;
+	int dow;
+	int hour;
+	int min;
+	int sec;
+} TimeAndDate;
+
+#endif
